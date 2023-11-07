3c3405496e101c47905674e749992c8bfb3bccec
Reimnop <rayngu2005@gmail.com>
2023-11-07 23:59:05 +0700

    Switching back to UMD...

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -8,7 +8,7 @@ export default [
         input: "src/index.ts",
         output: {
             dir: "build",
-            format: "esm",
+            format: "umd",
             name: "polyrhythmjs",
             sourcemap: true,
             globals: {
